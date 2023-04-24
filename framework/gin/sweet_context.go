91bb38908d366e096f68aa9da9ed4ab6adb87f7b
ranen <ranen1024@gmail.com>
2023-04-24 15:59:35 +0800

    feat: add gin

---
+++ framework/gin/sweet_context.go
@@ -0,0 +1,10 @@
+// Copyright 2023 ranen.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+package gin
+
+import "context"
+
+func (ctx *Context) BaseContext() context.Context {
+	return ctx.Request.Context()
+}
